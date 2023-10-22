49d770c9aa8cec422d821084c4651499af12edc2
Ironspidr <janatanielroshan@gmail.com>
2023-10-21 23:12:10 -0700

    login page fix

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,6 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'services/auth_service.dart';
 
+class MyLoginPage extends StatelessWidget {
+  const MyLoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Login Page',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
+
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
